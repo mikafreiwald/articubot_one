5b601ac4fcb4b3e56363426c065f4c488accc962
Mika Freiwald <mfreiwal@uni-bremen.de>
2025-05-05 14:08:27 +0200

    add y offset

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -70,7 +70,7 @@ def generate_launch_description():
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
                                    '-name', 'my_bot',
-                                   '-z', '0.1'],
+                                   '-z', '0.1', '-y', '0.4'],
                         output='screen')
 
 
